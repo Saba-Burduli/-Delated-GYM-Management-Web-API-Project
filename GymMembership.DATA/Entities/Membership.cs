629942a386d2bd59b6f0d73a7ede7f8ee3912926
Saba-Burduli <sabagg790@gmail.com>
2025-04-03 23:40:03 +0400

    Adding Enitity Classes

--- GymMembership.DATA/Entities/Membership.cs
+++ GymMembership.DATA/Entities/Membership.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GymMembership.DATA.Entities
 {
-    public class Person
+    public class Membership
     {
     }
 }
--- GymMembership.DATA/obj/Debug/net8.0/GymMembership.DATA.AssemblyInfo.cs
+++ GymMembership.DATA/obj/Debug/net8.0/GymMembership.DATA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acab3369d63ac33f8e133e89b2bbb0672354e78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f58c272b34d1e287f4d940bb4f212ed36a8e60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
