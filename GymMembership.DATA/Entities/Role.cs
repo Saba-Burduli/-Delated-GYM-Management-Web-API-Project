b3f4e402f41439cebe6f3360cf6f9863eaca4ef4
Saba-Burduli <sabagg790@gmail.com>
2025-04-03 23:53:24 +0400

    role enitity added

--- GymMembership.DATA/Entities/Role.cs
+++ GymMembership.DATA/Entities/Role.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GymMembership.DATA.Entities
 {
+    [Table("Role")]
     public class Role
     {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Required]
+        public string? RoleName { get; set; } //add seeding in there (Admin, Trainer, Member)
+
+        //Relations:
+
     }
 }
