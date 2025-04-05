62b96de083109378fe2e78ccaf1adc6e70f2127f
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-05 23:19:21 +0400

    Update Membership.cs

--- GymMembership.DATA/Entities/Membership.cs
+++ GymMembership.DATA/Entities/Membership.cs
@@ -19,7 +19,7 @@ namespace GymMembership.DATA.Entities
         public DateTime StartDate { get; set; } = DateTime.UtcNow; //we gonna test it now .. if it works i shouldnt add DateTime.Now
         
         [Required]
-        public DateTime EndDate { get; set; } = DateTime.UtcNow;//we gonna test it now .. if it works i shouldnt add DateTime.Now
+        public DateTime EndDate { get; set; } = DateTime.UtcNow; //we gonna test it now .. if it works i shouldnt add DateTime.Now
         
         [Required]
         public bool IsActive { get; set; }
