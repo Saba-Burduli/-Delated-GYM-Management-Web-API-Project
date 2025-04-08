c3cee697b2f6bbcf276eeefc406d6ed2e77d2628
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-08 23:34:18 +0400

    Update MembershipType.cs

--- GymMembership.DATA/Entities/MembershipType.cs
+++ GymMembership.DATA/Entities/MembershipType.cs
@@ -12,7 +12,6 @@ namespace GymMembership.DATA.Entities
     public class MembershipType
     {
         [Key]
-        
         public int MembershipTypeId { get; set; }
         [Required]
         public string? MembershipTypeName { get; set; } //Add seeding in there  (monthly, yearly, VIP)
