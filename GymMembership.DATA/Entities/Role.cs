cd7a52195f3cb99b49b86f48adbdc3fe9c6772bd
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-11 23:24:51 +0400

    Update Role.cs

--- GymMembership.DATA/Entities/Role.cs
+++ GymMembership.DATA/Entities/Role.cs
@@ -18,7 +18,6 @@ namespace GymMembership.DATA.Entities
         public string? RoleName { get; set; } //add seeding in there (Admin, Trainer, Member)
         
         //Relations:
-        
-        
+       
     }
 }
