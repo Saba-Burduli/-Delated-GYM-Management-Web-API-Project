d63bf2d35b4c2529e4ac611da55be61587007d38
SabaBurduli888 <sabagg790@gmail.com>
2025-04-17 18:50:09 +0400

    Adding Fluent API in MembershipConfiguration.cs

--- GymMembership.DATA/Configurations/MembershipConfiguration.cs
+++ GymMembership.DATA/Configurations/MembershipConfiguration.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using GymMembership.DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace GymMembership.DATA.Configurations
 {
-    public class MembershipConfiguration
+    public class MembershipConfiguration : IEntityTypeConfiguration<Membership>
     {
-        
+        public void Configure(EntityTypeBuilder<Membership> builder)
+        {
+         builder.HasKey(m=>m.MembershipTypeId);
+         builder.HasOne<User>(u => u.User)
+             .WithMany(m => m.Memberships)
+             .HasForeignKey("UserId")
+             .OnDelete(DeleteBehavior.Cascade);
+         builder.Property(m=>m.StartDate);
+         builder.Property(m=>m.EndDate);
+         builder.Property(m => m.IsActive);
+         builder.Property(m => m.Price);
+        }
     }
 }
