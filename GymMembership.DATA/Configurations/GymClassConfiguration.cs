cf845e15303eaa2f6f880034a0b5f3ca58fed30b
SabaBurduli888 <sabagg790@gmail.com>
2025-04-17 17:48:11 +0400

    Add GymClassConfiguration.cs And Delate GymClassUserConfiguration.cs

--- GymMembership.DATA/Configurations/GymClassConfiguration.cs
+++ GymMembership.DATA/Configurations/GymClassConfiguration.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GymMembership.DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace GymMembership.DATA.Configurations
 {
-    public class GymClassConfiguration
+    public class GymClassConfiguration : IEntityTypeConfiguration<GymClass>
     {
-        
+        public void Configure(EntityTypeBuilder<GymClass> builder)
+        {
+            builder.HasMany(gs => gs.GymClassUsers)
+                .WithOne(g => g.GymClass);
+        }
     }
 }
