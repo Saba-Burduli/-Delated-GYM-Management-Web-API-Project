23a17ec53554bcfe14705c1e5ee518edcfed2bde
SabaBurduli888 <sabagg790@gmail.com>
2025-04-17 19:12:38 +0400

    Adding IEntityTypeConfiguration In UserConfiguration.cs for More Fluent API

--- GymMembership.DATA/Configurations/UserConfiguration.cs
+++ GymMembership.DATA/Configurations/UserConfiguration.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GymMembership.DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace GymMembership.DATA.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
-        
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
