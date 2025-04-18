b2e708a62af1c93ce7991051cf9212f1bada9895
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-18 22:45:59 +0400

    Update MembershipConfiguration.cs

--- GymMembership.DATA/Configurations/MembershipConfiguration.cs
+++ GymMembership.DATA/Configurations/MembershipConfiguration.cs
@@ -14,15 +14,15 @@ namespace GymMembership.DATA.Configurations
     {
         public void Configure(EntityTypeBuilder<Membership> builder)
         {
-         builder.HasKey(m=>m.MembershipTypeId);
-         builder.HasOne<User>(u => u.User)
-             .WithMany(m => m.Memberships)
-             .HasForeignKey("UserId")
-             .OnDelete(DeleteBehavior.Cascade);
-         builder.Property(m=>m.StartDate);
-         builder.Property(m=>m.EndDate);
-         builder.Property(m => m.IsActive);
-         builder.Property(m => m.Price);
+             builder.HasKey(m=>m.MembershipTypeId);
+             builder.HasOne<User>(u => u.User)
+                 .WithMany(m => m.Memberships)
+                 .HasForeignKey("UserId")
+                 .OnDelete(DeleteBehavior.Cascade);
+             builder.Property(m=>m.StartDate);
+             builder.Property(m=>m.EndDate);
+             builder.Property(m => m.IsActive);
+             builder.Property(m => m.Price);
          
         }
     }
