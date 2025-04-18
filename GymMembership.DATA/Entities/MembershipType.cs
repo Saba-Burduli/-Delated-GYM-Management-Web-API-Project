528dc6fb4842e05f6fc019305988a7deb5fa508c
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-18 22:43:22 +0400

    Update MembershipType.cs

--- GymMembership.DATA/Entities/MembershipType.cs
+++ GymMembership.DATA/Entities/MembershipType.cs
@@ -22,6 +22,5 @@ namespace GymMembership.DATA.Entities
         //MembershipType => Membership ; One to One ; (MembeshipType) => (Membeship) 
         public virtual Membership? Membership { get; set; } //I dont think so I need this 
 
-
     }
 }
