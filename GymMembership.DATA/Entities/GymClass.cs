cb0bf5dbb08618854672b96508a1b3a1a81bbd9b
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-19 23:05:12 +0400

    Update GymClass.cs

--- GymMembership.DATA/Entities/GymClass.cs
+++ GymMembership.DATA/Entities/GymClass.cs
@@ -19,6 +19,5 @@ namespace GymMembership.DATA.Entities
 
         //GymClass => GymClassUsers ; One to Many ; (GymClass) => (GymClassUsers)
         public virtual ICollection<GymClassUsers>? GymClassUsers { get; set; }
-        
     }
 }
