aeef941d9acc803ab59328cd89ad1e4c845874b6
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-19 23:08:59 +0400

    Update GymClassConfiguration.cs

--- GymMembership.DATA/Configurations/GymClassConfiguration.cs
+++ GymMembership.DATA/Configurations/GymClassConfiguration.cs
@@ -15,7 +15,6 @@ namespace GymMembership.DATA.Configurations
         {
             builder.HasMany(gs => gs.GymClassUsers)
                 .WithOne(g => g.GymClass);       
-                
         }
     }
 }
