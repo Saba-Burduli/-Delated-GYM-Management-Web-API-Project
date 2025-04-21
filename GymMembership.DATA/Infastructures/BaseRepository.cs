0abbd03c8c700c2e54b006c60c35c6dd5b91c298
SabaBurduli888 <sabagg790@gmail.com>
2025-04-21 23:51:21 +0400

    Implement Base Methods in BaseRepository.cs

---
+++ GymMembership.DATA/Infastructures/BaseRepository.cs
@@ -0,0 +1,29 @@
+namespace GymMembership.DATA.Infastructures;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    public Task<IEnumerable<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIDAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
--- GymMembership.DATA/Infastructures/IBaseRepository.cs
+++ GymMembership.DATA/Infastructures/IBaseRepository.cs
@@ -1,6 +1,11 @@
 namespace GymMembership.DATA.Infastructures;
 
-public interface IBaseRepository
+public interface IBaseRepository<T> where T : class
 {
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T>GetByIDAsync(int id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
     
 }
