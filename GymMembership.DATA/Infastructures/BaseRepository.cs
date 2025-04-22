8fc00c28af4111f203ba871d3c49486fa43fe062
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-22 22:40:15 +0400

    Update BaseRepository.cs

--- GymMembership.DATA/Infastructures/BaseRepository.cs
+++ GymMembership.DATA/Infastructures/BaseRepository.cs
@@ -6,6 +6,7 @@ public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         throw new NotImplementedException();
     }
+    
 
     public Task<T> GetByIDAsync(int id)
     {
