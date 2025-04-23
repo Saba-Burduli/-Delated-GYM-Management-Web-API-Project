9d7076a4afa733b7ae8d1df3dd3b80a32d1eefcd
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-04-23 23:53:37 +0400

    Update BaseRepository.cs

--- GymMembership.DATA/Infastructures/BaseRepository.cs
+++ GymMembership.DATA/Infastructures/BaseRepository.cs
@@ -6,7 +6,7 @@ public class BaseRepository<T> : IBaseRepository<T> where T : class
 {
     private readonly GymMembershipDbContext _context;
     private readonly DbSet<T> _dbSet;
-    
+
     
     public BaseRepository(GymMembershipDbContext context)
     {
