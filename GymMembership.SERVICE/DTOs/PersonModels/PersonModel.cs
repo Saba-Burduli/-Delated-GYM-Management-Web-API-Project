f9c03405e21dbfe3baa1afdb85ecf8e19572e82e
SabaBurduli888 <sabagg790@gmail.com>
2025-04-26 22:18:01 +0400

    Adding in PersonModel.cs Some Properties From Main Entity

--- GymMembership.SERVICE/DTOs/PersonModels/PersonModel.cs
+++ GymMembership.SERVICE/DTOs/PersonModels/PersonModel.cs
@@ -2,5 +2,11 @@ namespace GymMembership.SERVICE.DTOs.PersonModels;
 
 public class PersonModel
 {
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
     
+    public string? Phone { get; set; }
+
+    public string? Address { get; set; }
 }
