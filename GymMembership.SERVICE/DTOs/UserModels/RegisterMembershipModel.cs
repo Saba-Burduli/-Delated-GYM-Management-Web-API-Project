9d0fbcdd285f5c7c4b9bed386d4f8b18cecd676d
SabaBurduli888 <sabagg790@gmail.com>
2025-04-26 22:35:08 +0400

    adding properties in RegisterMembershipModel.cs from (Membership Entity)

--- GymMembership.SERVICE/DTOs/UserModels/RegisterMembershipModel.cs
+++ GymMembership.SERVICE/DTOs/UserModels/RegisterMembershipModel.cs
@@ -2,5 +2,12 @@ namespace GymMembership.SERVICE.DTOs.UserModels;
 
 public class RegisterMembershipModel
 {
+    public int UserId { get; set; }
+
+    public int MembershipTypeId { get; set; }
+    
+    public DateTime StartDate { get; set; } = DateTime.UtcNow; //we gonna test it now .. if it works i shouldnt add DateTime.Now
+        
+    public DateTime EndDate { get; set; } = DateTime.UtcNow; //we gonna test it now .. if it works i shouldnt add DateTime.Now
     
 }
