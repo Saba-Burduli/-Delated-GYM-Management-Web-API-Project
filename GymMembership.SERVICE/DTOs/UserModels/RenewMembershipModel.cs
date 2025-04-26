bb1a74f737aea000cce5a3768a387aee0679bc9f
SabaBurduli888 <sabagg790@gmail.com>
2025-04-26 22:33:59 +0400

    Adding Properties inside RenewMembershipModel.cs from (Membership entity)

--- GymMembership.SERVICE/DTOs/UserModels/RenewMembershipModel.cs
+++ GymMembership.SERVICE/DTOs/UserModels/RenewMembershipModel.cs
@@ -2,5 +2,6 @@ namespace GymMembership.SERVICE.DTOs.UserModels;
 
 public class RenewMembershipModel
 {
-    
+    public int MembershipId { get; set; }
+    public DateTime NewEndDate { get; set; }
 }
