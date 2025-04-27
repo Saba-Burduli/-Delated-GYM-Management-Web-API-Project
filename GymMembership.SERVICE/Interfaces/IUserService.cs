1d470d39cb403c87cf99551511873f692c60e1ad
SabaBurduli888 <sabagg790@gmail.com>
2025-04-27 19:20:47 +0400

    Change Generic Members to Models and AuthResponse

--- GymMembership.SERVICE/Interfaces/IUserService.cs
+++ GymMembership.SERVICE/Interfaces/IUserService.cs
@@ -1,14 +1,16 @@
+using GymMembership.SERVICE.DTOs.UserModels;
+
 namespace GymMembership.SERVICE.Interfaces;
 
 public interface IUserService
 {
-    Task<UserModel> GetUserProfileAsync);
+    Task<UserModel> GetUserProfileAsync();
     Task<AuthResponseModel> UserRegistrationAsync(int? roleld, RegisterUserModel model);
-    Task<User> LoginAsync(string username, string password);
+    Task<AuthResponseModel> LoginAsync(string username, string password);
     Task<AuthResponseModel> UpdateUserProfileAsync(UpdateUserModel model);
     Task<AuthResponseModel> DeleteUserProfileAsync();
-    Task<AuthResponseModel,bool> AssignGymClassesAsync(List<int > gymClassIds); //maybe I'm gonna change Task<AuthResponseModel,bool>
+    Task<bool> AssignGymClassesAsync(List<int > gymClassIds); //maybe I'm gonna change Task<AuthResponseModel,bool>
                                                                                 //In this <> i Have AuthResponseModel,bool together and
                                                                                 //think i dont need this
-    List<GymClass> GetGymClassesByUserAsync(int userld);
+    List<GymClassModel> GetGymClassesByUserAsync(int userld);
 }
