61ade13f3a0ca8e4c17387a231977169f145c115
SabaBurduli888 <sabagg790@gmail.com>
2025-04-28 21:06:51 +0400

    Change Implemented methods in IMembershipService.cs

--- GymMembership.SERVICE/Interfaces/IMembershipService.cs
+++ GymMembership.SERVICE/Interfaces/IMembershipService.cs
@@ -1,10 +1,12 @@
+using GymMembership.SERVICE.DTOs.UserModels;
+
 namespace GymMembership.SERVICE.Interfaces;
 
 public interface IMembershipService
 {
-    Membership CreateMembership (Membership membership);
-    Task<bool> RenewMembership(int membershipld);
-    Task<MembershipStatus> GetMembershipStatusAsync(int customerId);
-    Task<List<Membership>> GetMembershipsByUser(int userld);
-    Task<List<Membership>> GetAllMembershipsByStatuses;
+    Task<MembershipModel> CreateMembership (RegisterMembershipModel membership);
+    Task RenewMembership(int membershipld);
+    Task<bool> GetMembershipStatusAsync(int customerId);
+    Task<List<MembershipModel>> GetMembershipsByUser(int userld);
+    Task<List<MembershipModel>> GetAllMembershipsByStatusesAsync();
 }
