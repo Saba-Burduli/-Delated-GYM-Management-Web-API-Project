08a7728142872c9861fed5ee5c46a7c3769aa1cb
SabaBurduli888 <sabagg790@gmail.com>
2025-04-29 21:48:07 +0400

    Change all 4 files (that files are not important and useful for Actuall Layers)

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30853b07d6e229f12b0a50d141ce2fdb48e0939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0a9528515cc75c0a439f8da5825e34a325b469")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
