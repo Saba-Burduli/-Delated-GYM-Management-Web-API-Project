6b2a8a4aca03b37f56020c3c3b6713a7e7531d54
SabaBurduli888 <sabagg790@gmail.com>
2025-04-29 22:12:33 +0400

    Adding ByMail Method in Repository

--- GymMembership.DAL/Repositories/IUserRepository.cs
+++ GymMembership.DAL/Repositories/IUserRepository.cs
@@ -1,9 +1,10 @@
 using GymMembership.DATA.Entities;
 using GymMembership.DATA.Infastructures;
+using GymMembership.SERVICE.DTOs.UserModels;
 
 namespace GymMembership.DAL.Repositories;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    
+    Task<UserModel>GetUserByEmail(string email);
 }
