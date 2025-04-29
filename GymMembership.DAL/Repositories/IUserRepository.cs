cdab1c0c6a65f975763e0eb6094759f0eea3201b
SabaBurduli888 <sabagg790@gmail.com>
2025-04-29 22:04:29 +0400

    Adding IUserRepository.cs

--- GymMembership.DAL/Repositories/IUserRepository.cs
+++ GymMembership.DAL/Repositories/IUserRepository.cs
@@ -3,30 +3,7 @@ using GymMembership.DATA.Infastructures;
 
 namespace GymMembership.DAL.Repositories;
 
-public class IUserRepository : IBaseRepository<User>
+public interface IUserRepository : IBaseRepository<User>
 {
-    public Task<IEnumerable<User>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
     
-    public Task<User> GetByIDAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
-    
-    public Task AddAsync(User entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task UpdateAsync(User entity)
-    {
-        throw new NotImplementedException();
-    }
-    
-    public Task DeleteAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
 }
