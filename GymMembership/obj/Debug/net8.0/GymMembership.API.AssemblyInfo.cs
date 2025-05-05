41dd3430ff842efb9e2e841ef819edf3d16b86cb
SabaBurduli888 <sabagg790@gmail.com>
2025-05-05 18:42:36 +0400

    adding methods for update whole project

--- GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
+++ GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998c9531bb498edff8db40cbfdb6391f80050fe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d23189e8c795689ff644df2f5176bb105c7841")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
