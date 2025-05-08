ca051466677a5714f9dbf5d033e041c6b111b8e9
SabaBurduli888 <sabagg790@gmail.com>
2025-05-08 18:49:37 +0400

    Add new fields to UpdateUserModel DTO
    Added properties for UserName, FirstName, LastName, Email, Phone, Address, and Password to the UpdateUserModel class. This enhances the model to support more comprehensive user updates.

--- GymMembership.SERVICE/DTOs/UserModels/UpdateUserModel.cs
+++ GymMembership.SERVICE/DTOs/UserModels/UpdateUserModel.cs
@@ -2,5 +2,18 @@ namespace GymMembership.SERVICE.DTOs.UserModels;
 
 public class UpdateUserModel
 {
+    public string? UserName { get; set; }
     
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+    
+    public string? Email { get; set; }
+    
+    public string? Phone { get; set; }
+    
+    public string? Address { get; set; }
+    
+    public string? Password { get; set; }
+
 }
