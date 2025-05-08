4130c4c9cb3e9a40829e36459f32e75449fe8210
SabaBurduli888 <sabagg790@gmail.com>
2025-05-08 18:49:50 +0400

    Update IUserService methods to require userId as a parameter
    Modified GetUserProfileAsync and UpdateUserProfileAsync to include a userId parameter. This ensures better specificity and allows operations to target the correct user.

--- GymMembership.SERVICE/Interfaces/IUserService.cs
+++ GymMembership.SERVICE/Interfaces/IUserService.cs
@@ -4,10 +4,10 @@ namespace GymMembership.SERVICE.Interfaces;
 
 public interface IUserService
 {
-    Task<UserModel> GetUserProfileAsync();
+    Task<UserModel> GetUserProfileAsync(int userld);
     Task<AuthResponseModel> UserRegistrationAsync(int? roleld, RegisterUserModel model);
     Task<AuthResponseModel> LoginAsync(string username, string password);
-    Task<AuthResponseModel> UpdateUserProfileAsync(UpdateUserModel model);
+    Task<AuthResponseModel> UpdateUserProfileAsync(UpdateUserModel model,int userId);
     Task<AuthResponseModel> DeleteUserProfileAsync();
     Task<bool> AssignGymClassesAsync(List<int > gymClassIds); //maybe I'm gonna change Task<AuthResponseModel,bool>
                                                                                 //In this <> i Have AuthResponseModel,bool together and
