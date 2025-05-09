16f337a7d17dee2f39421be3c5af044836d60244
SabaBurduli888 <sabagg790@gmail.com>
2025-05-09 23:16:08 +0400

    Add placeholder method for assigning gym classes
    Introduced a stub method `AssignGymClassesAsync` in `UserService` to handle gym class assignments for users. This prepares the service for future implementation of class assignment logic.

--- GymMembership.SERVICE/UserService.cs
+++ GymMembership.SERVICE/UserService.cs
@@ -63,6 +63,7 @@ public class UserService : IUserService
         throw new NotImplementedException();
     }//here i can use IUserRepository to delete user profile.
 
+    
     public Task<bool> AssignGymClassesAsync(List<int> gymClassIds)
     {
         throw new NotImplementedException();
