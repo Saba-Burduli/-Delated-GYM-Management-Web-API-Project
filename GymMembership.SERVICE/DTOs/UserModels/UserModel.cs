fef433b65ea7b7246fcc8e412c12ae8a8d4dc68f
SabaBurduli888 <sabagg790@gmail.com>
2025-05-09 23:15:50 +0400

    Remove unused RegistrationDate field from UserModel.
    The `RegistrationDate` property was removed as it is no longer utilized in the application. This change simplifies the `UserModel` class by eliminating unnecessary fields. No functional impact is expected from this update.

--- GymMembership.SERVICE/DTOs/UserModels/UserModel.cs
+++ GymMembership.SERVICE/DTOs/UserModels/UserModel.cs
@@ -3,12 +3,9 @@ namespace GymMembership.SERVICE.DTOs.UserModels;
 public class UserModel
 {
     public int UserId { get; set; }
-    
     public string? UserName { get; set; }
     
     public string? PasswordHash { get; set; }
-    
     public string? Email { get; set; }
-    
     public DateTime RegistrationDate { get; set; } = DateTime.Now;
 }
