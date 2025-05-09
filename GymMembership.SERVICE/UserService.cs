098b0b774b498a8c6782f78c8ef4115d9fa173f8
SabaBurduli888 <sabagg790@gmail.com>
2025-05-09 23:16:56 +0400

    Add GetGymClassesByUserAsync method placeholder
    Implemented the method definition for future functionality to retrieve gym classes by user ID. Currently throws a NotImplementedException as the logic is yet to be added.

--- GymMembership.SERVICE/UserService.cs
+++ GymMembership.SERVICE/UserService.cs
@@ -67,7 +67,6 @@ public class UserService : IUserService
     {
         throw new NotImplementedException();
     }//im gonna add GymClassRepository 
-    
     public List<GymClassModel> GetGymClassesByUserAsync(int userld)
     {
         throw new NotImplementedException();
