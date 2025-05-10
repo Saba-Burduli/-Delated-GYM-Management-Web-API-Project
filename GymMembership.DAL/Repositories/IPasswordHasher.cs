5e660cc661a01da8ab2304cc20ee9fbc02ded739
SabaBurduli888 <sabagg790@gmail.com>
2025-05-10 21:53:47 +0400

    Update assembly informational version
    Updated the AssemblyInformationalVersionAttribute to reflect the latest commit hash. This ensures the assembly metadata aligns with the new build.

--- GymMembership.DAL/Repositories/IPasswordHasher.cs
+++ GymMembership.DAL/Repositories/IPasswordHasher.cs
@@ -2,5 +2,19 @@ namespace GymMembership.DAL.Repositories;
 
 public interface IPasswordHasher
 {
-   
+    Task<string> HashPasswordAsync(string password);
+    Task<bool> VerifyPasswordHashAsync(string password, string passwordHash);
+}
+
+public class PasswordHasher : IPasswordHasher
+{
+    public Task<string> HashPasswordAsync(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> VerifyPasswordHashAsync(string password, string passwordHash)
+    {
+        throw new NotImplementedException();
+    }
 }
