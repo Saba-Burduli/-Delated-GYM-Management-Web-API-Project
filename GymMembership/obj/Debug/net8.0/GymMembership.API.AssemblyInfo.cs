b2126eeb691ad4d84d372ad26efb9f1d46854f36
SabaBurduli888 <sabagg790@gmail.com>
2025-05-10 21:50:41 +0400

    Update assembly informational version in API
    Updated the AssemblyInformationalVersionAttribute to reflect the new commit hash. This ensures versioning is consistent with the latest build changes. No functional code changes were made.

--- GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
+++ GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+878936d0be29831f99f2906c5ee87adfd89aa43c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd35b2c604f7a09d2d56c49c65c18b6c3767a339")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
