1cb897577b8d131a884a410131101cb6fb97195f
Saba Burduli <131397277+Saba-Burduli@users.noreply.github.com>
2025-05-12 21:31:45 +0400

    Update IBaseRepository.cs

--- GymMembership.DATA/Infastructures/IBaseRepository.cs
+++ GymMembership.DATA/Infastructures/IBaseRepository.cs
@@ -5,6 +5,7 @@ public interface IBaseRepository<T> where T : class
     Task<IEnumerable<T>> GetAllAsync();
     
     Task<T>GetByIDAsync(int id);
+    
     Task AddAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(int id);
