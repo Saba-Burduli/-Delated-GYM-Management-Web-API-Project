c279d8b69722bf7d85ea8bda8e839e3bffe25f47
SabaBurduli888 <sabagg790@gmail.com>
2025-05-19 20:27:13 +0400

    Update assembly informational version in build metadata
    Changed the AssemblyInformationalVersionAttribute to reflect the new build metadata hash. This update ensures proper version tracking for the application. No functional changes were made.

--- GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
+++ GymMembership/obj/Debug/net8.0/GymMembership.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d694d69dd0f694e0fbe97aaa02cd113c566e08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e21f1e0b21f509b2a84f0fdd2bb43cd350c7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
