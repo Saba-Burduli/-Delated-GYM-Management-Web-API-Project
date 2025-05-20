5fb5e82271488d97119c24f828867f3e2b1edb2d
SabaBurduli888 <sabagg790@gmail.com>
2025-05-20 22:31:18 +0400

    Add Email property to LoginUserModel
    This update introduces a new Email property to the LoginUserModel class. It enables handling email-based authentication or user identification alongside the existing properties.

--- GymMembership.SERVICE/DTOs/UserModels/LoginUserModel.cs
+++ GymMembership.SERVICE/DTOs/UserModels/LoginUserModel.cs
@@ -5,6 +5,8 @@ namespace GymMembership.SERVICE.DTOs.UserModels;
 public class LoginUserModel
 {
     public string? UserName { get; set; }
+    
+    public string? Email { get; set; }
 
     public string? Password { get; set; }
     
