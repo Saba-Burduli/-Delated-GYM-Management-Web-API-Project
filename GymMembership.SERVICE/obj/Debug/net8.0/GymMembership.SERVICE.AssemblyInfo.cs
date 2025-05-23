3cc6d517529a02fdb7e164158280a1fb61be74bc
SabaBurduli888 <sabagg790@gmail.com>
2025-05-24 00:31:08 +0400

    Update assembly informational version hash
    Updated the AssemblyInformationalVersionAttribute to reflect the new commit hash. This ensures the build metadata is in sync with the latest changes.

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dd965a3e8d1e53bd01970a9ccca215178c4fa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d1b56a3d76577545ba0951ac16b72717978db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
