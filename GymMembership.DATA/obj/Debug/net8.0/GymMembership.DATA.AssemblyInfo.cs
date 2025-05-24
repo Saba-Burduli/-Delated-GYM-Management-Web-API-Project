ce80146f4892cf0a5942ab82555152ee65d92d41
SabaBurduli888 <sabagg790@gmail.com>
2025-05-25 00:25:20 +0400

    Update assembly informational version in build metadata
    The assembly informational version was updated to reflect the latest build changes. This ensures consistency and traceability in the build process. No functional changes were made to the codebase.

--- GymMembership.DATA/obj/Debug/net8.0/GymMembership.DATA.AssemblyInfo.cs
+++ GymMembership.DATA/obj/Debug/net8.0/GymMembership.DATA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10290e1c7bf8d3f4d3d1ad270ddfe36d663d6cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c1bf7bea26be703cad6a0c99aa2580dd62bb66")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.DATA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
