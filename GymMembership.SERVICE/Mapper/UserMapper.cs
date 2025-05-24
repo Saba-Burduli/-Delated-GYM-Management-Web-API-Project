03e5abaaceeb71fa3e8550fdd0fc6b9e729a1332
SabaBurduli888 <sabagg790@gmail.com>
2025-05-24 16:19:18 +0400

    implementing first mapping method named GymClassModelMapping

--- GymMembership.SERVICE/Mapper/UserMapper.cs
+++ GymMembership.SERVICE/Mapper/UserMapper.cs
@@ -1,6 +1,17 @@
+using GymMembership.DATA.Entities;
+using GymMembership.SERVICE.DTOs.UserModels;
+
 namespace GymMembership.SERVICE.Mapper;
 
 public static class UserMapper
-{
-    
+{ 
+    //we need UserRolesMapping method
+    public static GymClassModel GymClassModelMapping(this GymClass entity)
+    {
+        return new GymClassModel()
+        {
+            GymClassId = entity.GymClassId,
+            GymClassName = entity.GymClassName,
+        };
+    }
 }
