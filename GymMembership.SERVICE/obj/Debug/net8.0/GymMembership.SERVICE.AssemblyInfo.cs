c501785db1d44cace75ef55376a9d27c7f5e0e28
SabaBurduli888 <sabagg790@gmail.com>
2025-05-26 20:48:49 +0400

    Update build artifacts and assembly informational versions
    Updated `AssemblyInformationalVersionAttribute` values across multiple projects to reflect the latest state. Additionally, regenerated NuGet and build cache files to ensure consistency with the current build.

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc528cfa93391dfb9a0aff230c0946734a4de28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d278bf712fdb88507aa25f90d3e12a78104a8f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
