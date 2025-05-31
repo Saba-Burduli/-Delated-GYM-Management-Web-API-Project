443a939093ec79b0c55e0b8bf6d2099c0aeb6231
SabaBurduli888 <sabagg790@gmail.com>
2025-05-31 21:13:06 +0400

    Update AssemblyInformationalVersion attribute
    Updated the AssemblyInformationalVersion to reflect the latest commit hash. This change ensures the version metadata aligns with the current build state. No functional changes were made.

--- GymMembership.DAL/obj/Debug/net8.0/GymMembership.DAL.AssemblyInfo.cs
+++ GymMembership.DAL/obj/Debug/net8.0/GymMembership.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997abb65061da138b87cec8fe3594b0f89779b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fe822ad5996ceb4c5e636635c1068d96e39d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
