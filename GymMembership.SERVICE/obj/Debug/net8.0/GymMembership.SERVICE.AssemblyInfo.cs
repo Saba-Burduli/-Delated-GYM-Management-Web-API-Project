a2bf4b177c1d778677d4e682a01299e4bfe2293d
Saba-Burduli <sabagg790@gmail.com>
2025-06-01 13:20:47 +0400

    Change project paths from Unix to Windows format
    Updated file paths and configurations to reflect a transfer of the development environment from a Unix-based system to a Windows-based system. Adjustments include path separators, fallback folders, and updated SDK versions.

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fe822ad5996ceb4c5e636635c1068d96e39d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded93d5a2639e9982bf376e869780091900ae14c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
