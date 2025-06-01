e7bd0bc6459485484769028e9651784ecbf83c17
Saba-Burduli <sabagg790@gmail.com>
2025-06-01 14:53:02 +0400

    Refactor UserRegistrationAsync to return Person object
    Modified the UserRegistrationAsync method to return a Person object instead of an AuthResponseModel. Removed unused legacy/commented code to improve code clarity and maintainability. Added a missing `using` directive for HttpHeaders.

--- GymMembership.SERVICE/Mapper/UserMapper.cs
+++ GymMembership.SERVICE/Mapper/UserMapper.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using GymMembership.DATA.Entities;
 using GymMembership.SERVICE.DTOs.UserModels;
 using GymMembership.SERVICE;
@@ -35,7 +36,7 @@ public static class UserMapper
     }
     
     // this is UserRegistationAsync Mapping in Mapping Directorie
-   public static Task<AuthResponseModel> UserRegistrationAsync(int? roleld, RegisterUserModel model)
+   public static async Task<Person> UserRegistrationAsync(int? roleld, RegisterUserModel model)
    {
        var person = new Person()
        {
@@ -45,38 +46,6 @@ public static class UserMapper
            Address = model.Person.Address
        };
        
-       return Task.FromResult(new AuthResponseModel { Success = true, Message = "Person Mapping Created" });
+       return await Task.FromResult(person);
    }
-   /*
-    *
-    *public class Person
-    {
-        [Key]
-        public int PersonId { get; set; }
-        
-        [Required]
-        [MaxLength(20, ErrorMessage = "FistName is more than 20 letter")]
-        public string? FirstName { get; set; }
-
-        [Required]
-        [MaxLength(30, ErrorMessage = "LastName is more than 30 letter")]
-        public string? LastName { get; set; }
-
-        [Required]
-        [MaxLength(20, ErrorMessage = "Phone number is more than 20 letter")]
-        public string? Phone { get; set; }
-
-        [Required]
-        [MaxLength(50, ErrorMessage = "address is more than 50 letter")]
-        public string? Address { get; set; }
-
-        //Relations :
-
-        //Person => User ; One to One ; (Person) => (User) but We can do One to Many(like Person can Have Many Users)   
-        public virtual User? User { get; set; }
-    }
-}
-    * 
-    */ 
-
 }
