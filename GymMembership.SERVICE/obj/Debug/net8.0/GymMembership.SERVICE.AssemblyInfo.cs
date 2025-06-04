f66d6a2159ddc55ef03ee84a375db04019205216
Saba-Burduli <sabagg790@gmail.com>
2025-06-04 19:13:32 +0400

    add gitignore inside project

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34140bc2701a8ff72c5f3a4dcd51cc52c741e842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f522c6d52be7a91a3b7fd4d4a9c4d52249768214")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
