f4cd65559719bfa2c5399d96b7536cdcb0e1f9e1
Saba-Burduli <sabagg790@gmail.com>
2025-06-07 23:56:15 +0400

    Update AssemblyInformationalVersion in AssemblyInfo.cs
    Updated the AssemblyInformationalVersionAttribute to reflect a new build identifier. This change helps ensure the assembly metadata matches the latest build state. No functional changes were introduced.

--- GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
+++ GymMembership.SERVICE/obj/Debug/net8.0/GymMembership.SERVICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a606bedcb535becca788422cc0bbd5332f7e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d96f941f8d1f29ddac44f9c7c99a8cb69ef8a2ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMembership.SERVICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
